8355c8fbeb67bc51ce95b37ced1e292c78b461da
Valeria <atargatis.the.one@gmail.com>
2013-07-15 11:17:15 +0300

    js fixes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -13,8 +13,7 @@ $(function () {
     $.randomImage = {
         defaults: {
 
-            //you can change these defaults to your own preferences.
-            path: 'http://gifs.gifbin.com/', //change this to the path of your images
+            path: 'http://gifs.gifbin.com/',
             myImages: ['052013/1372784443_dolphin_creates_bubble_rings.gif', '16014655g6.gif', '1239704331_gina-carano.gif', '032010/1270047760_kitten.gif' ] //put image names in this bracket. ex: 'harold.jpg', 'maude.jpg', 'etc'
 
         }
@@ -33,9 +32,6 @@ $(function () {
 
                 var imageNames = config.myImages;
 
-                //get size of array, randomize a number from this
-                // use this number as the array index
-
                 var imageNamesSize = imageNames.length;
 
                 var lotteryNumber = Math.floor(Math.random() * imageNamesSize);
@@ -44,9 +40,6 @@ $(function () {
 
                 var fullPath = config.path + winnerImage;
 
-
-                //put this image into DOM at class of randomImage
-                // alt tag will be image filename.
                 $(this).attr({
                     src: fullPath,
                     alt: winnerImage
--- app/assets/javascripts/remote_validation.js
+++ app/assets/javascripts/remote_validation.js
@@ -1,58 +1,61 @@
-( function( $ ) {
-  var RemoteValidation = {
-    defaults: {
-      messageContainer : '#error_messages',
-      messageHtml : "<div id='error_explanation'><h2>{{NUM_ERRORS}} errors prohibited this post from being saved:</h2>{{ERRORS}}</div>",
-      noErrorClass: 'field',
-      errorClass : 'field_with_errors'
-    },
-    showErrors: function(form, settings, errors) {
-      var errorList ="";
-      var formFor = form.attr('id').replace(/new_/,"");
-      var numErrors = 0;
-      for (var col in errors) {
-        for (var i in errors[col]) {
-          errorList += "<li> "+col+" "+errors[col][i]+"</li>";
-          $("*[name='"+formFor+"["+col+"]']", form).wrap($("<div/>").addClass(settings.errorClass));
-          //$("label[for='"+formFor+"_"+col+"']", form).wrap($("<div/>").addClass(settings.errorClass));
-          numErrors++;
-        }
-      }
-      //var errorHtml = settings.messageHtml.replace("{{NUM_ERRORS}}", numErrors);
-      //errorHtml = errorHtml.replace("{{ERRORS}}","<ul>"+errorList+"</ul>");
-      //$(settings.messageContainer).append(errorHtml);
-    }
-  };
-  $.fn.remoteValidation = function(options) {
-    var settings = $.extend( RemoteValidation.defaults, options);
-    return this.each( function() {
-      var $this = $(this); //$(this), may change inside function, so assign it to $this.
-      $this.on('ajax:beforeSend', function(evt,xhr) {
-        $(settings.messageContainer).empty();
-        $("."+settings.errorClass, $this).children().unwrap();
-      }).on('ajax:success', function(evt,data,status,xhr) {
-      }).on('ajax:error', function(evt, xhr, status, error) {
-      }).on('ajax:complete', function(evt, xhr, status){
-        var data = JSON.parse(xhr.responseText);
-        if (data.errors) {
-          RemoteValidation.showErrors($this, settings, data.errors);
-        } else if (data.location) {
-            if ($('title').html() == 'Step 1') {
-                $(this).hide();
-                $('title').html('Step 2');
-                $("#step_2").show();
+(function ($) {
+    var RemoteValidation = {
+        defaults: {
+            //messageContainer : '#error_messages',
+            //messageHtml : "<div id='error_explanation'><h2>{{NUM_ERRORS}} errors prohibited this post from being saved:</h2>{{ERRORS}}</div>",
+            noErrorClass: 'field',
+            errorClass: 'field_with_errors'
+        },
+        showErrors: function (form, settings, errors) {
+            var errorList = "";
+            var formFor = form.attr('id').replace(/new_/, "");
+            var numErrors = 0;
+            for (var col in errors) {
+                for (var i in errors[col]) {
+                    errorList += "<li> " + col + " " + errors[col][i] + "</li>";
+                    $("*[name='" + formFor + "[" + col + "]']", form).wrap($("<div/>").addClass(settings.errorClass));
+                    $("*[name='" + formFor + "[" + col + '(1i)' + "]']", form).wrap($("<div/>").addClass(settings.errorClass));
+                    $("*[name='" + formFor + "[" + col + '(2i)' + "]']", form).wrap($("<div/>").addClass(settings.errorClass));
+                    $("*[name='" + formFor + "[" + col + '(3i)' + "]']", form).wrap($("<div/>").addClass(settings.errorClass));
+                    //$("label[for='"+formFor+"_"+col+"']", form).wrap($("<div/>").addClass(settings.errorClass));
+                    numErrors++;
+                }
             }
-            else if ($('title').html() == 'Step 2') {
-                $(this).hide();
-                $('title').html('Step 3');
-                $("#step_3").show();
-                $('#countdown').countdown('pause');
-                window.localStorage.amount = $('.countdown_amount').text();
-                $('.countdown_amount').text(window.localStorage.amount);
-            }
+            //var errorHtml = settings.messageHtml.replace("{{NUM_ERRORS}}", numErrors);
+            //errorHtml = errorHtml.replace("{{ERRORS}}","<ul>"+errorList+"</ul>");
+            //$(settings.messageContainer).append(errorHtml);
         }
+    };
+    $.fn.remoteValidation = function (options) {
+        var settings = $.extend(RemoteValidation.defaults, options);
+        return this.each(function () {
+            var $this = $(this); //$(this), may change inside function, so assign it to $this.
+            $this.on('ajax:beforeSend',function (evt, xhr) {
+                //$(settings.messageContainer).empty();
+                $("." + settings.errorClass, $this).children().unwrap();
+            }).on('ajax:success',function (evt, data, status, xhr) {
+                }).on('ajax:error',function (evt, xhr, status, error) {
+                }).on('ajax:complete', function (evt, xhr, status) {
+                    var data = JSON.parse(xhr.responseText);
+                    if (data.errors) {
+                        RemoteValidation.showErrors($this, settings, data.errors);
+                    } else if (data.location) {
+                        if ($('title').html() == 'Step 1') {
+                            $(this).hide();
+                            $('title').html('Step 2');
+                            $("#step_2").show();
+                        }
+                        else if ($('title').html() == 'Step 2') {
+                            $(this).hide();
+                            $('title').html('Step 3');
+                            $("#step_3").show();
+                            $('#countdown').countdown('pause');
+                            window.localStorage.amount = $('.countdown_amount').text();
+                            $('.countdown_amount').text(window.localStorage.amount);
+                        }
+                    }
 
-      });
-    });
-  };
+                });
+        });
+    };
 })(jQuery);
